8d7a920890121b94897f215e16f9baffa626472a
dyekv <kogaktt@gmail.com>
2021-01-25 23:33:03 +0900

    add test sumofarrays

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,1 +1,22 @@
 // todo: ここに単体テストを書いてみましょう！
+import { sumOfArray, asyncSumOfArray, asyncSumOfArraySometimesZero } from "../functions";
+
+describe("sumOfArray", () => {
+  test("normal", () => {
+    expect(sumOfArray([1, 2])).toBe(3);
+  });
+  // 空の配列を渡すとテスト失敗（toThrowをしてもだめ）
+  // test("empty", () => {
+  //   expect(sumOfArray([])).toThrow();
+  // });
+  // 試しに書いたらコンパイルエラー
+  //   test("string", () => {
+  //     expect(sumOfArray(['string','string2'])).toThrow();
+  //   })
+});
+
+describe('asyncSumOfArray',()=>{
+  test('normal',async()=>{
+    await expect(asyncSumOfArray([1,2])).resolves.toBe(3);
+  })
+})
